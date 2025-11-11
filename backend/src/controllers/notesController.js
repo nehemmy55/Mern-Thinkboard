fbf0396628132d683302422c9620ca9738a81ac3
Nehemmy55 <n.ishimwe5@alustudent.com>
2025-11-11 16:10:20 +0200

    correct getAllNotes function and add error handling; export controller functions

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,16 +1,24 @@
- function getAllNotes(req,res){
-    res.status(200).send("you fetched  5 notes");
+import Note from '../models/Note.js';
+
+  export async function getAllNotes(req,res){
+    try {
+        const notes= await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error fetching notes:", error);  
+        res.status(500).json({message:"INternal Server Error"});
+    }
+
 }
 
- function createNote(req,res){
+  export  function createNote(req,res){
     res.status(201).json({message:"Note created successfully"});
 }
 
- function updateNote(req,res){
+  export function updateNote(req,res){
         res.status(200).json({message:"Note updated successfully"});
 }
 
-function deleteNote(req,res){
+export function deleteNote(req,res){
     res.status(200).json({message:"Note deleted successfully"});
 }
-module.exports={getAllNotes,createNote,updateNote,deleteNote};
